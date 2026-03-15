13ab8979dd64fddcc499c1d4dd04098f8926f2d6
Larrynuhuh <swastiksingh1402@gmail.com>
2026-03-15 18:41:48 +0530

    a

--- manifolds/vectors.py
+++ manifolds/vectors.py
@@ -11,25 +11,29 @@ def normal(g: Matrix, basis: Matrix) -> Vector:
     center = jnp.mean(basis, axis = 0)
     cbf = basis - center
 
+    sg = g + jnp.eye(g.shape[-1]) * 1e-12
     eigval, eigvect = jnp.linalg.eigh(g)
     s = eigval > 0
     seigval = jnp.where(s, jnp.sqrt(eigval), 0.0)
 
-    l = jnp.einsum('ik, k, jk -> ij', eigvect, seigval, eigvect)
+    l = jnp.linalg.cholesky(sg)
+    l = eigvect @ jnp.diag(seigval) @ eigvect.T
     
     cb = cbf @ l
 
     u, s, vh = jnp.linalg.svd(cb, full_matrices = False)
+    normal = vh[-1]
+    n_mani = jnp.linalg.solve(l.T, normal)
     raw_norm = vh[-1]
     n_mani = jnp.linalg.pinv(l.T) @ raw_norm
 
     check = mtc.iprod(g, n_mani, center)
     nrm = jnp.where(check < 0, -n_mani, n_mani)
-    return unitize(g, nrm)
+    return us.div(nrm, (mtc.norm(g, nrm)))
 
 @jax.jit
 def xnormal(g: Matrix, basis: Tensor) -> Matrix | Tensor: 
-    return jax.vmap(normal, in_axes=(None, 0))(g, basis)
+    return jax.vmap(normal, in_axes=(0, 0))(g, basis)
 
 #dot product territory
 @jax.jit
@@ -72,3 +76,7 @@ def xrejvect(g: Matrix, a: Matrix, b: Matrix) -> Matrix:
 @jax.jit
 def unitize(g: Matrix, u: Vector) -> Vector: 
     return us.div(u, mtc.norm(g, u))
+
+@jax.jit
+def xunitize(g: Matrix, u: Matrix) -> Matrix: 
+    return jax.vmap(unitize, in_axes=(None, 0))(g, u)
