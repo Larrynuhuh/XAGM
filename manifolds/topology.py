a26d08a253a670cbd9977b7acee38d94cc7c99cf
Larrynuhuh <swastiksingh1402@gmail.com>
2026-03-27 13:23:35 +0530

    tpg now takes in met field

--- manifolds/topology.py
+++ manifolds/topology.py
@@ -5,13 +5,16 @@ from manifolds import vectors as vct
 from geoutils import Vector, Matrix, Scalar, Tensor, JAXArray
 from basis import metrics as mtc
 
-def linlen(g: Matrix, l: Matrix) -> Scalar:
+
+def linlen(g_func, l: Matrix) -> Scalar:
     
     diff = jnp.diff(l, axis = 0)
-    lens = jax.vmap(mtc.norm, in_axes=(None, 0))(g, diff)
-    sums = jnp.sum(lens)
+    mp = us.div(l[:-1] + diff, 2.0)
+    gs = jax.vmap(g_func, in_axes=(0,))(mp)
 
-    return sums 
+    seglen = jax.vmap(mtc.norm, in_axes=(0, 0))(gs, diff)
+
+    return jnp.sum(seglen)
 
 
 # to check distance of point from line
@@ -30,11 +33,13 @@ def segdist(g: Matrix, f, h, pt):
 
 # USER CALLS PLDIST
 
-def pldist(g: Matrix, l: Matrix, pt: Vector) -> Scalar:
+def pldist(g, l: Matrix, pt: Vector) -> Scalar:
     a = l[:-1]
     b = l[1:]
+    midpoints = a + (b - a) / 2.0
+    gs = jax.vmap(g, in_axes=(0,))(midpoints)
 
-    curve_dist = jax.vmap(segdist, in_axes = (None, 0, 0, None))
-    summed = curve_dist(g, a, b, pt)
+    curve_dist = jax.vmap(segdist, in_axes = (0, 0, 0, None))
+    summed = curve_dist(gs, a, b, pt)
 
     return jnp.min(summed)
