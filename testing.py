19b8b3fd239b85efa20d9c66dc1d24bf295876ec
Larrynuhuh <swastiksingh1402@gmail.com>
2026-03-28 09:46:02 +0530

    yessir

--- testing.py
+++ testing.py
@@ -7,6 +7,10 @@ from basis import linear as lin
 from manifolds import vectors as vct
 from manifolds import topology as tpg
 from geoutils import Vector, Matrix, Scalar, Tensor, JAXArray
+import geoutils as us
 from numba import njit, prange
 import time
+
+
+
 
